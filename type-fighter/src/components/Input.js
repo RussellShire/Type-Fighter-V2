fbff33a546b1ddb9dc83900b14ceb934541a22f7
RussellShire <russell.shire@juicymedia.co.uk>
2023-02-15 20:46:57 +0000

    Add basic input component

---
+++ type-fighter/src/components/Input.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Input = () => {
+    const [text, setText] = useState('')
+
+    const onChange = (e) => {
+        setText(e)
+    }
+
+    return (
+        <>
+            <p>{text}</p>
+            <input
+                type='text'
+                className='form-control'
+                placeholder='Attack!'
+                value={text}
+                onChange={(e) => onChange(e.target.value)}
+                autoFocus
+            />
+        </>
+    )
+}
+
+export default Input;
